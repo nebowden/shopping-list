49479262ac4c0a666822102174f4ad44b5860549
nebowden <nathanebowden@gmail.com>
2014-11-10 18:15:42 -0600

    added jquery code to assign classes to list entries and to filter by class

--- js/app.js
+++ js/app.js
@@ -10,16 +10,173 @@ $(document).ready(function(){
 		$('.add-form').slideToggle();
 	});
 
-//adding input to list form
-	$('.add-item-button').click(function(){
+//clear text box
+	$(".text-entry").click(function(){
+		$(this).val('');
+	});
+//adding input to list form and assigning categories to added items
+var addItem = function(){
 		var item_name_input = $('.add-item-container').find("input[name='grocery-item']").val();
-		var item = $('<li><input type="checkbox" name="obtained">'+item_name_input+'<i class="fa fa-close"></i></li>');
+		var item = $('<li><input type="checkbox" name="obtained"><i class="fa fa-close"></i>'+item_name_input+'</li>');
 		$('.list-box > ul').prepend(item);
+		$('.text-entry').focus().val('');
+	};
+
+var assignCategory = function (){	
+	if ($("input[value='produce']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("produce-class");
+		};
+	if ($("input[value='dairy']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("dairy-class");
+		};
+	if ($("input[value='meat/deli']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("meat-deli-class");
+		};
+	if ($("input[value='frozen foods']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("frozen-foods-class");
+		};
+	if ($("input[value='dry goods']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("dry-goods-class");
+		};
+	if ($("input[value='pharmacy']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("pharmacy-class");
+		};
+	if ($("input[value='other']").is(":checked"))
+		{
+			$('.shopping-list li').first().addClass("other-class");
+		};
+	};
+
+$('.add-item-button').on('click', function(){
+	addItem();
+	assignCategory();
+	});
+
+$('.add-item-container').keydown(function (event){
+		if(event.which==13)
+		{
+		addItem();
+		assignCategory()};
 	});
 
 //graying out checked items
-	$("ul").on('click', 'li', function(){
-		$(this).toggleClass("checked");
+	$(".shopping-list").on('click', "input[name='obtained']", function(){
+		$(this).closest("li").toggleClass("checked");
 		});
+
+//removing list items
+	$(".shopping-list").on('click', ".fa-close", function(){
+		$(this).closest("li").remove();
+		});
+
+//close form for adding items
+$('.add-item-container').click(function(event) {
+	event.stopPropagation();
+});
+
+$(document).click(function(){
+	$('.add-form').slideUp();
+});
+
+//filtering by shopping category
+$('.show-produce').click(function(){
+	$('.produce-class').show();
+	$('.dairy-class').hide();
+	$('.meat-deli-class').hide();
+	$('.frozen-foods-class').hide();
+	$('.dry-goods-class').hide();
+	$('.pharmacy-class').hide();
+	$('.other-class').hide();
+});
+
+$('.show-all').click(function(){
+	$('.produce-class').show();
+	$('.dairy-class').show();
+	$('.meat-deli-class').show();
+	$('.frozen-foods-class').show();
+	$('.dry-goods-class').show();
+	$('.pharmacy-class').show();
+	$('.other-class').show();
+});
+
+$('.show-dairy').click(function(){
+	$('.produce-class').hide();
+	$('.dairy-class').show();
+	$('.meat-deli-class').hide();
+	$('.frozen-foods-class').hide();
+	$('.dry-goods-class').hide();
+	$('.pharmacy-class').hide();
+	$('.other-class').hide();
+});
+
+$('.show-meat').click(function(){
+	$('.produce-class').hide();
+	$('.dairy-class').hide();
+	$('.meat-deli-class').show();
+	$('.frozen-foods-class').hide();
+	$('.dry-goods-class').hide();
+	$('.pharmacy-class').hide();
+	$('.other-class').hide();
+});
+
+$('.show-frozen').click(function(){
+	$('.produce-class').hide();
+	$('.dairy-class').hide();
+	$('.meat-deli-class').hide();
+	$('.frozen-foods-class').show();
+	$('.dry-goods-class').hide();
+	$('.pharmacy-class').hide();
+	$('.other-class').hide();
+});
+
+$('.show-dry').click(function(){
+	$('.produce-class').hide();
+	$('.dairy-class').hide();
+	$('.meat-deli-class').hide();
+	$('.frozen-foods-class').hide();
+	$('.dry-goods-class').show();
+	$('.pharmacy-class').hide();
+	$('.other-class').hide();
+});
+
+$('.show-pharmacy').click(function(){
+	$('.produce-class').hide();
+	$('.dairy-class').hide();
+	$('.meat-deli-class').hide();
+	$('.frozen-foods-class').hide();
+	$('.dry-goods-class').hide();
+	$('.pharmacy-class').show();
+	$('.other-class').hide();
+});
+
+$('.show-other').click(function(){
+	$('.produce-class').hide();
+	$('.dairy-class').hide();
+	$('.meat-deli-class').hide();
+	$('.frozen-foods-class').hide();
+	$('.dry-goods-class').hide();
+	$('.pharmacy-class').hide();
+	$('.other-class').show();
+});
+
+//close show menu
+
+$('.show-options a').click(function(){
+	$('.show-options').slideUp();
+});
+
+$('.dropdown').click(function(event) {
+	event.stopPropagation();
+});
+
+$(document).click(function(){
+	$('.show-options').slideUp();
+});
 
 })
